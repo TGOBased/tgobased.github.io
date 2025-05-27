470b37190d49d50475b3822688cdec4aa608a99c
TGOBASEDVR <liamscott164@gmail.com>
2025-05-27 16:05:51 -0400

    Update 4399.js

--- subway/4399.js
+++ subway/4399.js
@@ -44,12 +44,17 @@ var my4399UnityModule = (function() {
                     if (!cache.override) return send.apply(self, sendArguments);
                     CachedXMLHttpRequest.log("'" + cache.requestURL + "' served from indexedDB cache (" + cache.response.byteLength + " bytes).");
                     if (xhr.onload) xhr.onload()
-                }
                 Object.defineProperty(self, "open", {
-                    value: (function(method, url, async) {
-                        cache = {
-                            method: method,
-                            requestURL: CachedXMLHttpRequest.cache.requestURL(url),
+  value: function(method, url, async) {
+    cache = {
+      method: method,
+      requestURL: CachedXMLHttpRequest.cache.requestURL(url),
+      async: async
+    };
+    return xhr.open.apply(xhr, arguments)
+  }
+});
+
                             async: async
                         };
                         return xhr.open.apply(xhr, arguments)
