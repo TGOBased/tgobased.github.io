640a2e539c934142339e5228a0930db93aae4446
TGOBASEDVR <liamscott164@gmail.com>
2025-05-27 16:19:56 -0400

    Update 4399.js

--- subway/4399.js
+++ subway/4399.js
@@ -1,17 +1,23 @@
 var my4399UnityModule = (function() {
-    var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
+    // Get the script directory URL if available
+    var _scriptDir = (typeof document !== 'undefined' && document.currentScript) ? document.currentScript.src : undefined;
+
     return (function(UnityModule) {
         UnityModule = UnityModule || {};
         var Module = typeof UnityModule !== "undefined" ? UnityModule : {};
-        if (typeof ENVIRONMENT_IS_PTHREAD === "undefined" || !ENVIRONMENT_IS_PTHREAD) {
-            function CachedXMLHttpRequest() {
-                var self = this,
-                    xhr = new CachedXMLHttpRequest.XMLHttpRequest,
-                    cache = {};
 
+        // Only define CachedXMLHttpRequest if not a pthread environment
+        if (typeof ENVIRONMENT_IS_PTHREAD === "undefined" || !ENVIRONMENT_IS_PTHREAD) {
+
+            function CachedXMLHttpRequest() {
+                var self = this;
+                var xhr = new CachedXMLHttpRequest.XMLHttpRequest();  // instantiate underlying XMLHttpRequest
+                var cache = {};
+
+                // Send method wrapper to handle caching logic
                 function send() {
                     var onload = xhr.onload;
-                    xhr.onload = (function(e) {
+                    xhr.onload = function(e) {
                         var meta = {
                             requestURL: cache.requestURL,
                             responseURL: xhr.responseURL,
@@ -19,85 +25,159 @@ var my4399UnityModule = (function() {
                             lastModified: xhr.getResponseHeader("Last-Modified"),
                             eTag: xhr.getResponseHeader("ETag")
                         };
-                        if (xhr.status == 200 && (meta.lastModified || meta.eTag)) {
+
+                        if (xhr.status === 200 && (meta.lastModified || meta.eTag)) {
+                            // Cache the response in IndexedDB
                             meta.size = xhr.response.byteLength;
-                            CachedXMLHttpRequest.cache.put(cache.requestURL, meta, xhr.response, (function(err) {
+                            CachedXMLHttpRequest.cache.put(cache.requestURL, meta, xhr.response, function(err) {
                                 CachedXMLHttpRequest.log("'" + cache.requestURL + "' downloaded successfully (" + xhr.response.byteLength + " bytes) " + (err ? "but not stored in indexedDB cache due to error." : "and stored in indexedDB cache."));
-                                if (onload) onload(e)
-                            }))
+                                if (onload) onload(e);
+                            });
                         } else {
-                            if (xhr.status == 304) {
+                            // Status 304 means Not Modified - serve from cache
+                            if (xhr.status === 304) {
                                 cache.override = true;
-                                CachedXMLHttpRequest.log("'" + cache.requestURL + "' served from indexedDB cache (" + cache.response.byteLength + " bytes).")
+                                CachedXMLHttpRequest.log("'" + cache.requestURL + "' served from indexedDB cache (" + cache.response.byteLength + " bytes).");
                             }
-                            if (onload) onload(e)
+                            if (onload) onload(e);
                         }
-                    });
-                    return xhr.send.apply(xhr, arguments)
+                    };
+                    return xhr.send.apply(xhr, arguments);
                 }
 
+                // Helper for revalidating cross-origin cache
                 function revalidateCrossOriginRequest(meta, self, sendArguments) {
-                    var headXHR = new CachedXMLHttpRequest.XMLHttpRequest;
-                    headXHR.open("HEAD", meta.requestURL, false);
+                    var headXHR = new CachedXMLHttpRequest.XMLHttpRequest();
+                    headXHR.open("HEAD", meta.requestURL, false);  // synchronous HEAD request
                     headXHR.send();
-                    cache.override = meta.lastModified ? meta.lastModified == headXHR.getResponseHeader("Last-Modified") : meta.eTag && meta.eTag == headXHR.getResponseHeader("ETag");
-                    if (!cache.override) return send.apply(self, sendArguments);
+
+                    // Compare lastModified or eTag headers to decide if override allowed
+                    cache.override = meta.lastModified ? 
+                        (meta.lastModified === headXHR.getResponseHeader("Last-Modified")) :
+                        (meta.eTag && meta.eTag === headXHR.getResponseHeader("ETag"));
+
+                    if (!cache.override) {
+                        // Cache is stale, do actual send
+                        return send.apply(self, sendArguments);
+                    }
                     CachedXMLHttpRequest.log("'" + cache.requestURL + "' served from indexedDB cache (" + cache.response.byteLength + " bytes).");
-                    if (xhr.onload) xhr.onload()
+                    if (xhr.onload) xhr.onload();
                 }
+
+                // Define open() method
                 Object.defineProperty(self, "open", {
-                    value: (function(method, url, async) {
+                    value: function(method, url, async) {
                         cache = {
                             method: method,
                             requestURL: CachedXMLHttpRequest.cache.requestURL(url),
                             async: async
                         };
-                        return xhr.open.apply(xhr, arguments)
-                    })
+                        return xhr.open.apply(xhr, arguments);
+                    }
                 });
+
+                // Define setRequestHeader()
                 Object.defineProperty(self, "setRequestHeader", {
-                    value: (function() {
+                    value: function() {
                         cache.customHeaders = true;
-                        return xhr.setRequestHeader.apply(xhr, arguments)
-                    })
+                        return xhr.setRequestHeader.apply(xhr, arguments);
+                    }
                 });
+
+                // Define send()
                 Object.defineProperty(self, "send", {
-                    value: (function(data) {
+                    value: function(data) {
                         var sendArguments = arguments;
-                        var absoluteUrlMatch = cache.requestURL.match("^https?://[^/]+/");
-                        if (!absoluteUrlMatch || cache.customHeaders || data || cache.method != "GET" || !cache.async || xhr.responseType != "arraybuffer") return xhr.send.apply(xhr, sendArguments);
-                        CachedXMLHttpRequest.cache.get(cache.requestURL, (function(err, result) {
-                            if (err || !result || !result.meta || result.meta.responseType != xhr.responseType) return send.apply(self, sendArguments);
+
+                        var absoluteUrlMatch = cache.requestURL.match(/^https?:\/\/[^\/]+\//);
+                        // Conditions where cache is bypassed:
+                        // custom headers, any data in send, method not GET, not async, or wrong responseType
+                        if (!absoluteUrlMatch || cache.customHeaders || data || cache.method !== "GET" || !cache.async || xhr.responseType !== "arraybuffer") {
+                            return xhr.send.apply(xhr, sendArguments);
+                        }
+
+                        // Try getting from cache first
+                        CachedXMLHttpRequest.cache.get(cache.requestURL, function(err, result) {
+                            if (err || !result || !result.meta || result.meta.responseType !== xhr.responseType) {
+                                return send.apply(self, sendArguments);
+                            }
                             cache.status = 200;
                             cache.statusText = "OK";
                             cache.response = result.response;
                             cache.responseURL = result.meta.responseURL;
-                            if (window.location.href.lastIndexOf(absoluteUrlMatch[0], 0)) return revalidateCrossOriginRequest(result.meta, self, sendArguments);
-                            if (result.meta.lastModified) xhr.setRequestHeader("If-Modified-Since", result.meta.lastModified);
-                            else if (result.meta.eTag) xhr.setRequestHeader("If-None-Match", result.meta.eTag);
+
+                            // If the cached URL's base does not match current location's base, revalidate cross-origin
+                            if (window.location.href.lastIndexOf(absoluteUrlMatch[0], 0) !== 0) {
+                                return revalidateCrossOriginRequest(result.meta, self, sendArguments);
+                            }
+
+                            // Set headers to allow conditional GET
+                            if (result.meta.lastModified) {
+                                xhr.setRequestHeader("If-Modified-Since", result.meta.lastModified);
+                            } else if (result.meta.eTag) {
+                                xhr.setRequestHeader("If-None-Match", result.meta.eTag);
+                            }
                             xhr.setRequestHeader("Cache-Control", "no-cache");
-                            return send.apply(self, sendArguments)
-                        }))
-                    })
+
+                            return send.apply(self, sendArguments);
+                        });
+                    }
                 });
-                ["abort", "getAllResponseHeaders", "getResponseHeader", "overrideMimeType", "addEventListener"].forEach((function(method) {
+
+                // Pass through methods to underlying xhr
+                ["abort", "getAllResponseHeaders", "getResponseHeader", "overrideMimeType", "addEventListener"].forEach(function(method) {
                     Object.defineProperty(self, method, {
-                        value: (function() {
-                            return xhr[method].apply(xhr, arguments)
-                        })
-                    })
-                }));
-                ["readyState", "response", "responseText", "responseType", "responseURL", "responseXML", "status", "statusText", "timeout", "upload", "withCredentials", "onloadstart", "onprogress", "onabort", "onerror", "onload", "ontimeout", "onloadend", "onreadystatechange"].forEach((function(property) {
+                        value: function() {
+                            return xhr[method].apply(xhr, arguments);
+                        }
+                    });
+                });
+
+                // Proxy properties from xhr, fallback to cached override if set
+                ["readyState", "response", "responseText", "responseType", "responseURL", "responseXML", "status", "statusText", "timeout", "upload", "withCredentials", "onloadstart", "onprogress", "onabort", "onerror", "onload", "ontimeout", "onloadend", "onreadystatechange"].forEach(function(property) {
                     Object.defineProperty(self, property, {
-                        get: (function() {
-                            return cache.override && cache[property] ? cache[property] : xhr[property]
-                        }),
-                        set: (function(value) {
-                            xhr[property] = value
-                        })
-                    })
-                }))
+                        get: function() {
+                            return cache.override && cache[property] !== undefined ? cache[property] : xhr[property];
+                        },
+                        set: function(value) {
+                            xhr[property] = value;
+                        }
+                    });
+                });
             }
+
+            // Make the XMLHttpRequest available on CachedXMLHttpRequest (needed externally)
+            CachedXMLHttpRequest.XMLHttpRequest = XMLHttpRequest;
+
+            // Placeholder for cache and log (should be defined elsewhere)
+            CachedXMLHttpRequest.cache = {
+                requestURL: function(url) {
+                    // Normalize URL or just return as is
+                    return url;
+                },
+                get: function(url, callback) {
+                    // Placeholder cache get method
+                    callback(null, null);
+                },
+                put: function(url, meta, response, callback) {
+                    // Placeholder cache put method
+                    callback(null);
+                }
+            };
+
+            CachedXMLHttpRequest.log = function(message) {
+                if (console && console.log) console.log("[CachedXHR] " + message);
+            };
+
+            // Attach CachedXMLHttpRequest constructor to Module for external access
+            Module.CachedXMLHttpRequest = CachedXMLHttpRequest;
+        }
+
+        // Return the module object
+        return Module;
+    })(my4399UnityModule);
+})();
+
             CachedXMLHttpRequest.XMLHttpRequest = window.XMLHttpRequest;
             CachedXMLHttpRequest.log = (function(message) {
                 if (Module.CachedXMLHttpRequestSilent !== true) console.log("[CachedXMLHttpRequest] " + message)
